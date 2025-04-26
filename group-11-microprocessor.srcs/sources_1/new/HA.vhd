dbef9510b05c46cdbc2f9055102afbca7d0d5642
Sahithyan <sahithyan2701@gmail.com>
2025-04-26 14:53:21 +0530

    add HalfAdder

---
+++ group-11-microprocessor.srcs/sources_1/new/HA.vhd
@@ -0,0 +1,31 @@
+----------------------------------------------------------------------------------
+-- Engineer: Sahithyan K.
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity HA is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           S : out STD_LOGIC;
+           C : out STD_LOGIC);
+end HA;
+
+architecture Behavioral of HA is
+
+begin
+	S <= A xor B;
+	C <= A and B;
+
+end Behavioral;
