15e78262f6b0121fc619a1b688feb68f84ecf5d1
Suhas Dissanayake <suhasdissa@gmail.com>
2025-04-29 18:46:25 +0530

    Add Multiplier_2

---
+++ group-11-microprocessor.srcs/sources_1/new/Multiplier_2.vhd
@@ -0,0 +1,50 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY Multiplier_2 IS
+    PORT (
+        a : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+        b : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+        y : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        C_in : IN STD_LOGIC);
+END Multiplier_2;
+
+ARCHITECTURE Behavioral OF Multiplier_2 IS
+    COMPONENT FA
+        PORT (
+            A : IN STD_LOGIC;
+            B : IN STD_LOGIC;
+            C_in : IN STD_LOGIC;
+            S : OUT STD_LOGIC;
+            C_out : OUT STD_LOGIC);
+    END COMPONENT;
+
+    SIGNAL A0B0, A1B0, A1B1, A0B1 : STD_LOGIC;
+    SIGNAL C1 : STD_LOGIC;
+
+BEGIN
+
+    A0B0 <= A(0) AND B(0);
+    A1B0 <= A(1) AND B(0);
+    A1B1 <= A(1) AND B(1);
+    A0B1 <= A(0) AND B(1);
+
+    FA_0 : FA
+    PORT MAP(
+        A => A1B0,
+        B => A0B1,
+        C_in => C_in,
+        S => Y(1),
+        C_Out => C1);
+
+    FA_1 : FA
+    PORT MAP(
+        A => '0',
+        B => A1B1,
+        C_in => C1,
+        S => Y(2),
+        C_Out => Y(3));
+
+    Y(0) <= A0B0;
+
+END Behavioral;
