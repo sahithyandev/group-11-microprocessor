3c4a611d0f68963bf2bdb37ec2b76284a61ac3ce
Suhas Dissanayake <suhasdissa@gmail.com>
2025-05-03 11:50:19 +0530

    Add 4 bit advanced register

---
+++ group-11-microprocessor.srcs/sources_1/new/Register_4_advanced.vhd
@@ -0,0 +1,42 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY Register4bit IS
+    PORT (
+        clk : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        load : IN STD_LOGIC;
+        d : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        q : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+    );
+END Register4bit;
+
+ARCHITECTURE Structural OF Register4bit IS
+
+    COMPONENT D_FF
+        PORT (
+            clk : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            d : IN STD_LOGIC;
+            q : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    SIGNAL d_internal : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL q_internal : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+BEGIN
+
+    d_internal(0) <= (d(0) AND load) OR (NOT(load) AND q_internal(0));
+    d_internal(1) <= (d(1) AND load) OR (NOT(load) AND q_internal(1));
+    d_internal(2) <= (d(2) AND load) OR (NOT(load) AND q_internal(2));
+    d_internal(3) <= (d(3) AND load) OR (NOT(load) AND q_internal(3));
+
+    FF0 : D_FF PORT MAP(clk => clk, reset => reset, d => d_internal(0), q => q_internal(0));
+    FF1 : D_FF PORT MAP(clk => clk, reset => reset, d => d_internal(1), q => q_internal(1));
+    FF2 : D_FF PORT MAP(clk => clk, reset => reset, d => d_internal(2), q => q_internal(2));
+    FF3 : D_FF PORT MAP(clk => clk, reset => reset, d => d_internal(3), q => q_internal(3));
+
+    q <= q_internal;
+
+END Structural;
