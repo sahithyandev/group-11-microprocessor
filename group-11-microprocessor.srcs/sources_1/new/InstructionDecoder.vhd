881519bd54ef7be87c9e5c233e1dd0c7ebd6907c
Suhas Dissanayake <suhasdissa@gmail.com>
2025-05-06 19:48:05 +0530

    Add some instruction decoding logic

--- group-11-microprocessor.srcs/sources_1/new/InstructionDecoder.vhd
+++ group-11-microprocessor.srcs/sources_1/new/InstructionDecoder.vhd
@@ -18,6 +18,62 @@ ENTITY InstructionDecoder IS
 END InstructionDecoder;
 
 ARCHITECTURE Behavioral OF InstructionDecoder IS
+    COMPONENT Decoder_2_to_4
+        PORT (
+            I : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+            EN : IN STD_LOGIC;
+            Y : OUT STD_LOGIC_VECTOR (3 DOWNTO 0));
+    END COMPONENT;
+
+    SIGNAL INSTRUCTION_CODE : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL INSTRUCTION_DECODED : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+    SIGNAL R_A, R_B : STD_LOGIC_VECTOR(2 DOWNTO 0); -- Register addresses
+    SIGNAL D : STD_LOGIC_VECTOR(3 DOWNTO 0); -- Immediate value (can be 3 bits to 4 bits long)
+
+    SIGNAL IS_MOVI, IS_ADD, IS_NEG, IS_JZR : STD_LOGIC;
 
 BEGIN
+    INSTRUCTION_CODE <= INSTRUCTION_IN(11 DOWNTO 10); -- first two bits represent the instruction opcode (ie: bits are indexed from right-0 to left-11)
+    R_A <= INSTRUCTION_IN(9 DOWNTO 7); -- next 3 bits represent register A address
+    R_B <= INSTRUCTION_IN(6 DOWNTO 4); -- next 3 bits represetn register B address
+    D <= INSTRUCTION_IN(3 DOWNTO 0); -- next 4 bits represent immediate value for (MOVI) or jump address for (JZR)
+
+    Decoder_2_4_0 : Decoder_2_to_4
+    PORT MAP(
+        I => INSTRUCTION_CODE,
+        EN => '1',
+        Y => INSTRUCTION_DECODED
+    );
+
+    IS_MOVI <= INSTRUCTION_DECODED(2);
+    IS_ADD <= INSTRUCTION_DECODED(0);
+    IS_NEG <= INSTRUCTION_DECODED(1);
+    IS_JZR <= INSTRUCTION_DECODED(3);
+
+    -- MOVI
+    REG_EN <= R_A; -- Enable the first register for write
+    LOAD_SEL <= '1'; -- set load source to immedate value
+    IMMEDIATE_VAL <= D; -- set immedate value from instruction
+
+    -- ADD
+    REG_SEL_A <= R_A; -- set first register as the first input of RCA
+    REG_SEL_B <= R_B; -- set secnd register as second input of RCA
+    REG_EN <= R_A; -- enable first register for write
+    ADD_SUB_SEL <= '0'; -- set ADD mode
+    LOAD_SEL <= '0'; --set load source to RCA
+
+    -- NEG
+    REG_SEL_A <= "000"; -- set 0 as the first input of RCA
+    REG_SEL_B <= R_B; -- set secnd register as second input of RCA
+    REG_EN <= R_A; -- enable first register for write
+    ADD_SUB_SEL <= '1'; -- set SUBTRACT mode
+    LOAD_SEL <= '0'; --set load source to RCA
+
+    -- JZR
+    REG_SEL_A <= R_A; -- set first register as the REG_CHK
+    JMP_ADDR <= D(2 DOWNTO 0); -- SET JMP address
+    JMP_FLAG <= '1' WHEN REG_CHK_JMP = "0000" ELSE
+        '0'; -- set JMP flag if register is 0000
+
 END Behavioral;
