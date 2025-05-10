f242ab7f2c0b753b85a810a08374627e3ad1f7ac
ShenethFd <shenethchalindauom@gmail.com>
2025-05-10 11:52:38 +0530

    TB_Mux_4bit_2way.vhd

--- group-11-microprocessor.srcs/sim_1/new/TB_Mux_4bit_2way.vhd
+++ group-11-microprocessor.srcs/sim_1/new/TB_Mux_4bit_2way.vhd
@@ -18,13 +18,12 @@ ARCHITECTURE Behavioral OF TB_mux2x1_4Bit IS
             A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
             B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
             sel : IN STD_LOGIC;
-            en : IN STD_LOGIC;
             Y : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
         );
     END COMPONENT;
 
     SIGNAL A, B, Y : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL sel, en : STD_LOGIC;
+    SIGNAL sel : STD_LOGIC;
 
 BEGIN
     UUT : Mux_4bit_2way
@@ -32,39 +31,32 @@ BEGIN
         A => A,
         B => B,
         sel => sel,
-        en => en,
         Y => Y
     );
 
-    PROCESS
-    BEGIN
-        --Test Case(01)|Selecting A [ A:='0101' B:='1110' sel:=0 en:=1 (output should be A, which is 0001)];
+    Process
+    Begin
+        --Test Case(01)|Selecting A [ A:='0101' B:='1110' sel:=0  (output should be A, which is 0101)];
         A <= "0101";
         B <= "1110";
         sel <= '0';
-        en <= '1';
-        WAIT FOR 100 ns;
-
-        --Test Case(02)|Selecting B [ A:='0101' B:='1110' sel:=1 en:=1 (output should be B, which is 1110)];
+        wait for 100ns;
+        
+        --Test Case(02)|Selecting B [ A:='0101' B:='1110' sel:=1  (output should be B, which is 1110)];
         sel <= '1';
-        WAIT FOR 100 ns;
-
-        --Test Case(03)|Check Enable Signal [ A:='0101' B:='1110' sel:=1 en:=0 (output should be 0000)];
-        en <= '0';
-        WAIT FOR 100 ns;
-
-        -- Test Case(04)|New Inputs,Check MUX again [A:='1101' B:='1001' sel:=0 en:=1 (output should be A, which is 1101)]
-        A <= "1101";
-        B <= "1001";
-        en <= '1';
+        wait for 100ns;
+          
+        -- Test Case(03)|New Inputs,Check MUX again [A:='1101' B:='1001' sel:=0  (output should be A, which is 1101)]
+        A   <= "1101"; 
+        B   <= "1001"; 
         sel <= '0';
-        WAIT FOR 100 ns;
-
-        -- Test Case(05)|New Inputs,Check MUX again [A:='1101' B:='1001' sel:=1 en:=1 (output should be B, which is 1001)]
+        wait for 100 ns;
+        
+       -- Test Case(04)|New Inputs,Check MUX again [A:='1101' B:='1001' sel:=1 en:=1 (output should be B, which is 1001)]
         sel <= '1';
-        WAIT FOR 100 ns;
-        WAIT;
-
-    END PROCESS;
+        wait for 100 ns;
+        wait;
+        
+        end process;
 
 END Behavioral;
